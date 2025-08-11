ee31e6517c70eeebffefb11e7299bc242fa5af89
MrDevelop636 <matix2025x@protonmail.com>
2025-08-11 22:59:26 +0200

    Automatic build commit

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,8 @@ import viteImagemin from "vite-plugin-imagemin";
 export default defineConfig({
   plugins: [
     viteImagemin({
-      gifsicle: { optimizationLevel: 7 },
-      optipng: { optimizationLevel: 7 },
-      mozjpeg: { quality: 80 },
-      pngquant: { quality: [0.7, 0.9], speed: 4 },
-      svgo: {},
       webp: {
-        quality: 80,
+        quality: 10,
       },
     }),
   ],
