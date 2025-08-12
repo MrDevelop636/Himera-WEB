9e728fed3d04563bb1f2b9810046183f21bd5794
MrDevelop636 <matix2025x@protonmail.com>
2025-08-12 12:07:42 +0200

    Automatic build commit

--- vite.config.js
+++ vite.config.js
@@ -29,24 +29,20 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: resolve(__dirname, "dist"),
+    outDir: resolve(__dirname, "dist"),   // ważne - folder wyjściowy
     emptyOutDir: true,
     assetsInlineLimit: 0,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "./src/pages/index.html"),
-        about: resolve(__dirname, "./src/pages/about.html"),
-        services: resolve(__dirname, "./src/pages/services.html"),
+      index: resolve(__dirname, 'index.html'),
+      about: resolve(__dirname, '/pages/about.html'),
+      services: resolve(__dirname, '/pages/services.html'),
+    }
       },
       output: {
         assetFileNames: (assetInfo) => {
+          // Pobieramy rozszerzenie (ostatnią część po kropce)
           const extType = assetInfo.name.split(".").pop();
-          
-          // Specjalna obsługa plików HTML - trafią do głównego folderu dist
-          if (/html/i.test(extType)) {
-            return "[name][extname]";
-          }
-          
           if (/png|jpe?g|svg|gif|webp|avif|ico|bmp|tiff/i.test(extType)) {
             return "assets/images/[name]-[hash][extname]";
           }
@@ -71,4 +67,4 @@ export default defineConfig({
       },
     },
   },
-});
+);
