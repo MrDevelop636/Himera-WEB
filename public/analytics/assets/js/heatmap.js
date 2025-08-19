eb4a1240dec825a6818f5deae582b9ae413bfe22
MrDevelop636 <matix2025x@protonmail.com>
2025-08-19 12:59:57 +0200

    Automatic build commit

--- public/analytics/assets/js/heatmap.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('click', (e) => {
-    const page = window.location.pathname;
-    const x = e.pageX;
-    const y = e.pageY;
-
-    navigator.sendBeacon('/analytics/heatmap.php', new URLSearchParams({
-        page, x, y
-    }));
-});
